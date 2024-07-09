ef22398588ed79b99f3abd88f5d41e7cb95c7b68
Daniel Vigano <unb10@toledobrasil.com>
2024-07-09 17:28:16 -0300

    Adicionar arquivos de projeto.

---
+++ src/Journey.Application/UseCases/Trips/GetAll/GetAllTripUseCase.cs
@@ -0,0 +1,27 @@
+﻿using Journey.Communication.Responses;
+using Journey.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Application.UseCases.Trips.GetAll
+{
+    public class GetAllTripUseCase
+    {
+        public ResponseTripsJson Execute()
+        {
+            var dbContext = new JourneyDbContext();
+
+            var trips = dbContext.Trips.AsNoTracking().ToList();
+
+            return new ResponseTripsJson
+            {
+                Trips = trips.Select(trip => new ResponseShortTripJson { 
+                    Id = trip.Id,
+                    Name = trip.Name,
+                    EndDate = trip.EndDate,
+                    StartDate = trip.StartDate,
+                }).ToList(),
+            };
+
+        }
+    }
+}
---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -0,0 +1,61 @@
+﻿using Journey.Communication.Requests;
+using Journey.Communication.Responses;
+using Journey.Exception;
+using Journey.Exception.ExceptionsBase;
+using Journey.Infrastructure;
+using Journey.Infrastructure.Entities;
+
+namespace Journey.Application.UseCases.Trips.Register
+{
+    public class RegisterTripUseCase
+    {
+        public ResponseShortTripJson Execute(RequestRegisterTripJson req) {
+
+            Validate(req);
+
+            var dbContext = new JourneyDbContext();
+
+            var entity = new Trip
+            {
+                Name = req.Name,
+                EndDate = req.EndDate,
+                StartDate = req.StartDate,
+            };
+
+            dbContext.Add(entity);
+            dbContext.SaveChanges();
+
+            return new ResponseShortTripJson
+            {
+                Name = entity.Name,
+                EndDate = entity.EndDate,
+                StartDate = entity.StartDate,
+                Id = entity.Id,
+            };
+            
+        }
+
+        private void Validate(RequestRegisterTripJson req) {
+
+            if (string.IsNullOrWhiteSpace(req.Name)) {
+
+                throw new JourneyException(ResourceErrorMessages.NAME_EMPTY);
+            
+            }
+
+            if (req.StartDate.Date < DateTime.UtcNow.Date) {
+
+                throw new JourneyException(ResourceErrorMessages.DATE_TRIP_MUST_BE_LATER_THAN_TODAY);
+
+            }
+
+            if (req.EndDate.Date < req.StartDate.Date)
+            {
+
+                throw new JourneyException(ResourceErrorMessages.END_DATE_TRIP_MUST_BE_LATER_START_DATE);
+
+            }
+
+        }
+    }
+}
